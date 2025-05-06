1a7101c41e3ac321a6995b86ffd183d79867408c
ElPetra <kursorr@gmail.com>
2025-05-06 23:44:15 +0300

    updated

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,8 +1,9 @@
+import React from "react";
 import "./Article.css";
 
 export const Article = ({ id, symbol, title, text }) => {
   return (
-    <article className="article" id = {id}>
+    <article className="article" id={id}>
       <h2 className="title">{symbol}</h2>
       <h3 className="title__second">{title}</h3>
       <p className="content">{text}</p>
