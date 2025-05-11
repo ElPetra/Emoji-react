b55e1d0c5feab71d9fd87a4f7d41a06ba510d14f
ElPetra <kursorr@gmail.com>
2025-05-11 13:14:56 +0300

    Updates

---
+++ precache-manifest.64cee3588de226bb60c2fee035b3be39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16771943389af1d9de8c7881e3c5d29",
+    "url": "/Emoji-react/index.html"
+  },
+  {
+    "revision": "4b7e86f26bd175217872",
+    "url": "/Emoji-react/static/css/main.96d35644.chunk.css"
+  },
+  {
+    "revision": "7c35ef4723ffe4a7a896",
+    "url": "/Emoji-react/static/js/2.0b9b69ad.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Emoji-react/static/js/2.0b9b69ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1559f6f2bf42323dfb79",
+    "url": "/Emoji-react/static/js/3.8429047d.chunk.js"
+  },
+  {
+    "revision": "4b7e86f26bd175217872",
+    "url": "/Emoji-react/static/js/main.2af840b9.chunk.js"
+  },
+  {
+    "revision": "005ba0ff756f93fa8eea",
+    "url": "/Emoji-react/static/js/runtime-main.9fecaafa.js"
+  }
+]);
